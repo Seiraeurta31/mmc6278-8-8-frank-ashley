5733b1ecac2ad38ccbc9595219a3bf7eadd2a5c0
Ashley Frank <ashley.frank@ufl.edu>
2023-10-28 13:57:16 -0400

    updated user, tag, comment model

--- models/Comment.js
+++ models/Comment.js
@@ -4,7 +4,7 @@ const CommentSchema = new Schema({
 // Create an "author" property with type String and default of 'Anonymous'
 author: {
     type: String,
-    default: ("Anonymous")
+    default: 'Anonymous'
 },
 // Create a "body" property with type String and make it required
 body: {
@@ -14,7 +14,7 @@ body: {
 // Create a "createdAt" property with type Date and set default to Date.now
 createdAt: {
     type: Date,
-    default: Date.now()
+    default: Date.now
 }
 })
 
